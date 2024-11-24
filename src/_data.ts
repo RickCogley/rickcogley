0afd4f5694a4ad8269ec4fec156531a2dc6ad888
James R. Cogley <rick.cogley@esolia.co.jp>
2024-11-24 13:09:15 +0900

    remove tz

--- src/_data.ts
+++ src/_data.ts
@@ -1,4 +1,3 @@
-Deno.env.set("TZ", "Asia/Tokyo");
 // Simple constants
 export const cacheBuster = `${new Date().getTime()}`;
 export const todaysDateYYYYMMDD = `${new Date().toISOString('ja-JP', { timeZone: 'Asia/Tokyo' }).split("T")[0]}`;
