c10ae6ad3216b59b59feaa45f2400cf29a5ae6e2
James R. Cogley <rick.cogley@esolia.co.jp>
2024-11-24 12:47:25 +0900

    try setting tz in config

--- src/_data.ts
+++ src/_data.ts
@@ -1,3 +1,6 @@
+// Set default timezone for Date
+Deno.env.set("TZ", "US/Eastern");
+
 // Simple constants
 export const cacheBuster = `${new Date().getTime()}`;
 export const todaysDateYYYYMMDD = `${new Date().toISOString('ja-JP', { timeZone: 'Asia/Tokyo' }).split("T")[0]}`;
