3dae0aba4d9d73625b2b60ad0681bd07c8fa3466
James R. Cogley <rick.cogley@esolia.co.jp>
2024-11-24 20:07:04 +0900

    Add code to get lume version

--- _config.ts
+++ _config.ts
@@ -1,5 +1,6 @@
 import lume from "lume/mod.ts";
 import plugins from "./plugins.ts";
+import { getCurrentVersion } from "lume/core/utils/lume_version.ts";
 
 const site = lume({
     src: "./src",
@@ -7,6 +8,7 @@ const site = lume({
 });
 
 site.use(plugins());
+site.data("lumeVersion", getCurrentVersion());
 
 site.ignore("README.md");
 site.ignore("*.DS_Store");
